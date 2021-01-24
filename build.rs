d1dd06f4fe563a2f772664e7ca7506562e6e5842
2ndTaleStudio <43264484+2ndTaleStudio@users.noreply.github.com>
2021-01-24 17:45:09 +0100

    bring core exeption handling to boot crate

--- build.rs
+++ build.rs
@@ -12,24 +12,24 @@ extern crate cc;
 use std::env;
 
 fn main() {
-    let script_location = env::current_dir().unwrap();
+  let script_location = env::current_dir().unwrap();
 
-    if let Some(target_arch) = env::var_os("CARGO_CFG_TARGET_ARCH") {
-        if target_arch == "aarch64" {
-            cc::Build::new()
-                .file("src/asm/aarch64/bootstrap.S")
-                .flag("-march=armv8-a")
-                .compile("bootstrap");
-            cc::Build::new()
-                .file("src/asm/aarch64/exceptionvector.S")
-                .flag("-march=armv8-a")
-                .compile("excvector");
-            // print the linker file location of the boot crate to the env-variables
-            println!("cargo:linkerscript={}/link64.ld", script_location.display());
+  if let Some(target_arch) = env::var_os("CARGO_CFG_TARGET_ARCH") {
+    if target_arch == "aarch64" {
+      cc::Build::new()
+        .file("src/asm/aarch64/bootstrap.S")
+        .flag("-march=armv8-a")
+        .compile("bootstrap");
+      cc::Build::new()
+        .file("src/asm/aarch64/exceptionvector.S")
+        .flag("-march=armv8-a")
+        .compile("excvector");
+      // print the linker file location of the boot crate to the env-variables
+      println!("cargo:linkerscript={}/link64.ld", script_location.display());
 
-            println!("cargo:rerun-if-changed=link64.ld");
-            println!("cargo:rerun-if-changed=src/asm/aarch64/bootstrap.S");
-            println!("cargo:rerun-if-changed=src/asm/aarch64/exceptionvector.S");
-        }
+      println!("cargo:rerun-if-changed=link64.ld");
+      println!("cargo:rerun-if-changed=src/asm/aarch64/bootstrap.S");
+      println!("cargo:rerun-if-changed=src/asm/aarch64/exceptionvector.S");
     }
+  }
 }
